3c032f5aab88f383b77139582c72052da9739650
Cardin Menkemeller <41063241+cmenkemeller@users.noreply.github.com>
2024-06-08 14:28:54 -0400

    Added basic animation for movie tiles

--- lib/widgets/movie_list_tile.dart
+++ lib/widgets/movie_list_tile.dart
@@ -5,34 +5,77 @@ import 'package:equifax_movie_db/widgets/poster_image.dart';
 import 'package:equifax_movie_db/widgets/title_subtitle_text.dart';
 import 'package:flutter/material.dart';
 
-class MovieListTile extends StatelessWidget {
+class MovieListTile extends StatefulWidget {
   final Movie movie;
   final EdgeInsets? padding;
+
   const MovieListTile({required this.movie, this.padding, super.key});
+
+  @override
+  State<MovieListTile> createState() => _MovieListTileState();
+}
+
+class _MovieListTileState extends State<MovieListTile>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _opacityAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+
+    _slideAnimation =
+        Tween<Offset>(begin: const Offset(0.2, 0.0), end: Offset.zero).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => DetailsFeature(id: movie.id.toString()).push(context),
+      onTap: () => DetailsFeature(id: widget.movie.id.toString()).push(context),
       child: Padding(
-        padding: padding ?? const EdgeInsets.all(0),
+        padding: widget.padding ?? const EdgeInsets.all(0),
         child: IntrinsicHeight(
           child: Row(children: [
-            PosterImage(posterPath: movie.posterPath ?? ''),
+            PosterImage(posterPath: widget.movie.posterPath ?? ''),
             const SizedBox(width: 8),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TitleSubtitleText(title: 'Title:', subtitle: movie.title),
-                if (movie.releaseDate != null)
-                  TitleSubtitleText(
-                      title: 'Release Date:',
-                      subtitle: movie.releaseDate ?? ''),
-                TitleSubtitleText(
-                    title: 'Average Rating:',
-                    subtitle: movie.voteAverage.toString()),
-              ],
+            SlideTransition(
+              position: _slideAnimation,
+              child: FadeTransition(
+                opacity: _opacityAnimation,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TitleSubtitleText(
+                        title: 'Title:', subtitle: widget.movie.title),
+                    if (widget.movie.releaseDate != null)
+                      TitleSubtitleText(
+                          title: 'Release Date:',
+                          subtitle: widget.movie.releaseDate ?? ''),
+                    TitleSubtitleText(
+                        title: 'Average Rating:',
+                        subtitle: widget.movie.voteAverage.toString()),
+                  ],
+                ),
+              ),
             ),
           ]),
         ),
